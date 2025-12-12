0ccb71e58734a568c605b8a71e38bfcb76025a82
BenedictoGeraldo <geraldodawa@gmail.com>
2025-12-12 20:04:47 +0700

    update day 2: use state on form

--- src/day2_state_hooks/01_UseStateExample.jsx
+++ src/day2_state_hooks/01_UseStateExample.jsx
@@ -1,28 +1,78 @@
 import { useState } from "react";
 
-function CounterExample() {
-  const [count, setCount] = useState(0);
+// function CounterExample() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div className="px-2">
+//       <h2>Counter: {count}</h2>
+
+//       {/* increment*/}
+//       <button className="mx-2" onClick={() => setCount(count + 1)}>
+//         Tambah (+1)
+//       </button>
+
+//       {/* decrement */}
+//       <button className="mx-2" onClick={() => setCount(count - 1)}>
+//         Kurang (-1)
+//       </button>
+
+//       {/* reset */}
+//       <button className="mx-2" onClick={() => setCount(0)}>
+//         Reset
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default CounterExample;
+
+function FormExample() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [semester, setSemester] = useState("");
+
+  const handleSubmit = () => {
+    alert(`Nama: ${name}, Umur: ${age}, Semester: ${semester}`);
+  };
 
   return (
-    <div className="px-2">
-      <h2>Counter: {count}</h2>
+    <div>
+      <h2>Form untuk menguji penggunaan use state pada form</h2>
 
-      {/* increment*/}
-      <button className="mx-2" onClick={() => setCount(count + 1)}>
-        Tambah (+1)
-      </button>
+      <div>
+        <label>Nama: </label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Masukan Nama"
+        />
+      </div>
 
-      {/* decrement */}
-      <button className="mx-2" onClick={() => setCount(count - 1)}>
-        Kurang (-1)
-      </button>
+      <div>
+        <label>Umur: </label>
+        <input
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          placeholder="Masukan Umur"
+        />
+      </div>
 
-      {/* reset */}
-      <button className="mx-2" onClick={() => setCount(0)}>
-        Reset
-      </button>
+      <div>
+        <label>Semester: </label>
+        <input
+          type="number"
+          value={semester}
+          onChange={(e) => setSemester(e.target.value)}
+          placeholder="Masukan Semester"
+        />
+      </div>
+
+      <button onClick={handleSubmit}>Kirim Form </button>
     </div>
   );
 }
 
-export default CounterExample;
+export default FormExample;
