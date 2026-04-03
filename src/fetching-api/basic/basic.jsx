49ec252e02deede4582ce064bfbb850b425415ac
BenedictoGeraldo <geraldodawa@gmail.com>
2026-04-03 13:40:23 +0700

    learn: fetching api basic with error handling and loading

--- src/fetching-api/basic/basic.jsx
+++ src/fetching-api/basic/basic.jsx
@@ -2,16 +2,27 @@ import { useEffect, useState } from "react";
 
 export default function Basic() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Terjadi kesalahan saat mengambil data");
+        }
+        return res.json();
+      })
+      .then((data) => setPosts(data))
+      .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <div>
       <h1>Belajar Get API From JSONPlaceholder</h1>
+
+      {loading && <p>Loading...</p>}
 
       {posts.map((post) => (
         <div key={post.id}>
@@ -20,6 +31,8 @@ export default function Basic() {
           <hr />
         </div>
       ))}
+
+      {error && <p>{error}</p>}
     </div>
   );
 }
